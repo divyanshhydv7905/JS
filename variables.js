84df9d5405e11bc2391812ce502c5f24dbf6a220
divyanshhydv7905 <dyadav.9648@gmail.com>
2026-06-26 21:03:11 +0530

    Day 1 learning

---
+++ variables.js
@@ -0,0 +1,10 @@
+const accountId= 144553
+let accountEmail= "yadav@google.com"
+var accountPassword= "1234"
+accountCity= "kanpur"
+
+accountEmail ="bsdk.com"
+accountPassword="2143"
+accountCity ="jaipur"
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
