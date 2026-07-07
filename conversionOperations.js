483f8f9ad7aabb4feea4b708e19d0f3866e17de7
divyanshhydv7905 <dyadav.9648@gmail.com>
2026-07-07 23:08:33 +0530

    strings and comparisons

---
+++ conversionOperations.js
@@ -0,0 +1,9 @@
+// let val = 3
+// let negVAl = -val
+// console.log(negVAl)
+
+//------------------------Comparisons------------------------------------------
+console.log("2">1) // js converts it into number but sometimes not accurate
+console.log(null==0) 
+console.log(null>=0) // here the null is treated as a number 0
+console.log("2"===2)
---
+++ strings.js
@@ -0,0 +1,7 @@
+const name= "Divyansh"
+const repoCount= 50
+const clgname= "AITM"
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount} currently studying in ${clgname}`);
+const newstring = clgname.substring(0,2)
+console.log(newstring)
