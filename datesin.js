a35b1bd08e33b6806bca2a92dfe587df73285044
divyanshhydv7905 <dyadav.9648@gmail.com>
2026-07-09 13:54:07 +0530

    Date and time in js

---
+++ datesin.js
@@ -0,0 +1,10 @@
+//Dates
+
+let myDate= new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.toISOString())
+console.log(myDate.toTimeString())
+console.log(myDate.toDateString())
+console.log(typeof  myDate)
